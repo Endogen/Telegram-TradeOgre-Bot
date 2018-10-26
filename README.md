613dc26b5381a0bc0fcc25748969c2ebb0ded9a6
Endogen <081081@gmx.net>
2018-10-26 20:27:08 +0200

    Charts: removed depricated method and added msg if no data available

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
 #### Run in background
-`nohup python3 START.py &`
+```
+nohup python3 START.py &
+```
 
 #### Bring back to foreground
-`ps ax | grep START.py`
+```
+ps ax | grep START.py
+```
+
+#### Start with
+```
+python3.6 -m tradeogrebot.START
+```
+
+#### For installation of `orca`
+```
+https://github.com/plotly/orca
+```
+
+#### Install `orca` with
+```
+sudo npm install -g electron@1.8.4 orca
+```
+or with [Conda](https://conda.io/miniconda.html)
+```
+conda install -c plotly plotly-orca psutil
+```
+
+#### First make sure to have installed
+```
+sudo apt-get install python3.6-dev
+```
+
+#### Make sure to have this installed also
+```
+apt-get install libgconf-2-4
+```
+
+#### If error `xvfb-run: error:Xvfb failed to start` 
+```
+xvfb-run -a /path/to/orca "$@"
+```
--- tradeogrebot/plugins/chart.py
+++ tradeogrebot/plugins/chart.py
@@ -1,13 +1,13 @@
 import io
 import pandas as pd
 import plotly.io as pio
+import plotly.figure_factory as fif
 import tradeogrebot.emoji as emo
 import tradeogrebot.labels as lbl
 
 from io import BytesIO
 from telegram import ParseMode
 from telegram.ext import RegexHandler
-from plotly.tools import FigureFactory as FiFa
 from tradeogrebot.plugin import TradeOgreBotPlugin
 from tradeogrebot.api.cryptocompare import CryptoCompare
 
@@ -37,13 +37,19 @@ class Chart(TradeOgreBotPlugin):
 
         data = CryptoCompare().historical_ohlcv_hourly(to_sy, from_sy, self.TIME_FRAME)["Data"]
 
+        if not data:
+            update.message.reply_text(
+                text=f"No OHLC data available for {to_sy} {emo.OH_NO}",
+                parse_mode=ParseMode.MARKDOWN)
+            return
+
         o = [value["open"] for value in data]
         h = [value["high"] for value in data]
         l = [value["low"] for value in data]
         c = [value["close"] for value in data]
         t = [value["time"] for value in data]
 
-        fig = FiFa.create_candlestick(o, h, l, c, pd.to_datetime(t, unit='s'))
+        fig = fif.create_candlestick(o, h, l, c, pd.to_datetime(t, unit='s'))
         fig['layout']['yaxis'].update(tickformat="0.4r")
         fig['layout'].update(title=f"Price of {to_sy} in {from_sy} for {days} days")
 
