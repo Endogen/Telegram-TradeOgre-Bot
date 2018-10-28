13fd8d810a3b7b3ad8b274f1b728f7fb17df62c9
Endogen <081081@gmx.net>
2018-10-28 23:51:31 +0100

    Added `back` plugin to be able to get back to main menu after restart

---
+++ tradeogrebot/plugins/back.py
@@ -0,0 +1,13 @@
+import tradeogrebot.labels as lbl
+
+from telegram.ext import RegexHandler
+from tradeogrebot.plugin import TradeOgreBotPlugin
+
+
+class Back(TradeOgreBotPlugin):
+
+    def get_handlers(self):
+        return [self._get_back_handler()]
+
+    def _get_back_handler(self):
+        return RegexHandler(f"^({lbl.BTN_BACK})$", self.back)
