1f79b5aa69ab9c279315b540c6c99a3788e1a53f
Endogen <081081@gmx.net>
2018-10-30 12:31:48 +0100

    Add EUR value and coinlib link to stats and make api call threaded

--- tradeogrebot/plugins/stats.py
+++ tradeogrebot/plugins/stats.py
@@ -1,3 +1,4 @@
+import threading
 import tradeogrebot.labels as lbl
 
 from coinmarketcap import Market
@@ -7,6 +8,10 @@ from tradeogrebot.plugin import TradeOgreBotPlugin
 
 
 class Stats(TradeOgreBotPlugin):
+
+    coin_id = ""
+    data_btc = ""
+    data_eur = ""
 
     def get_handlers(self):
         return [self._get_stats_handler()]
@@ -18,25 +23,31 @@ class Stats(TradeOgreBotPlugin):
     @TradeOgreBotPlugin.check_pair
     @TradeOgreBotPlugin.send_typing_action
     def _stats(self, bot, update, data):
-        coins = data.pair.split("-")
+        coin = data.pair.split("-")[1]
         listings = Market().listings()
 
-        coin_id = None
         for listing in listings["data"]:
-            if coins[1].upper() == listing["symbol"].upper():
-                coin_id = listing["id"]
+            if coin.upper() == listing["symbol"].upper():
+                self.coin_id = listing["id"]
                 break
 
-        ticker = Market().ticker(coin_id, convert=coins[0])
+        thread_usd = threading.Thread(target=self.market_btc())
+        thread_eur = threading.Thread(target=self.market_eur())
 
-        coin = ticker["data"]
-        symbol = coin["symbol"]
-        slug = coin["website_slug"]
-        rank = str(coin["rank"])
-        sup_c = "{0:,}".format(int(coin["circulating_supply"]))
-        sup_t = "{0:,}".format(int(coin["total_supply"]))
+        thread_usd.start()
+        thread_eur.start()
 
-        usd = coin["quotes"]["USD"]
+        thread_usd.join()
+        thread_eur.join()
+
+        btc = self.data_btc["data"]
+        symbol = btc["symbol"]
+        slug = btc["website_slug"]
+        rank = str(btc["rank"])
+        sup_c = "{0:,}".format(int(btc["circulating_supply"]))
+        sup_t = "{0:,}".format(int(btc["total_supply"]))
+
+        usd = btc["quotes"]["USD"]
         p_usd = "{0:.8f}".format(usd["price"])
         v_24h = "{0:,}".format(int(usd["volume_24h"]))
         m_cap = "{0:,}".format(int(usd["market_cap"]))
@@ -44,17 +55,32 @@ class Stats(TradeOgreBotPlugin):
         c_24h = str(usd["percent_change_24h"])
         c_7d = str(usd["percent_change_7d"])
 
-        btc = coin["quotes"][coins[0]]
+        btc = btc["quotes"]["BTC"]
         p_btc = "{0:.8f}".format(float(btc["price"]))
 
+        eur = self.data_eur["data"]["quotes"]["EUR"]
+        p_eur = "{0:.8f}".format(float(eur["price"]))
+
         update.message.reply_text(
-            text=f"`{symbol} {p_usd} USD | {p_btc} {coins[0]}\n"
-                 f"1h {c_1h}% | 24h {c_24h}% | 7d {c_7d}%\n\n"
+            text=f"`{symbol}\n\n"
+                 f"{p_usd} USD\n"
+                 f"{p_eur} EUR\n"
+                 f"{p_btc} BTC\n\n"
+                 f"1h  {c_1h}%\n"
+                 f"24h {c_24h}%\n"
+                 f"7d  {c_7d}%\n\n"
                  f"CMC Rank: {rank}\n"
                  f"Volume 24h: {v_24h} USD\n"
                  f"Market Cap: {m_cap} USD\n"
                  f"Circ. Supp: {sup_c} {symbol}\n"
                  f"Total Supp: {sup_t} {symbol}`\n\n"
-                 f"[Stats from CoinMarketCap](https://coinmarketcap.com/currencies/{slug})",
+                 f"[Stats on CoinMarketCap](https://coinmarketcap.com/currencies/{slug})\n"
+                 f"[Stats on Coinlib](https://coinlib.io/coin/{coin}/{coin})",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True)
+
+    def market_btc(self):
+        self.data_btc = Market().ticker(self.coin_id, convert="BTC")
+
+    def market_eur(self):
+        self.data_eur = Market().ticker(self.coin_id, convert="EUR")
