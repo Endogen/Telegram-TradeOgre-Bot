cefce56e8c47d5cc45997f1698d8f36c61ddadc1
Endogen <081081@gmx.net>
2018-11-06 10:56:16 +0100

    Removed unwanted back-commandhandler

--- tradeogrebot/plugins/back.py
+++ tradeogrebot/plugins/back.py
@@ -7,10 +7,7 @@ from tradeogrebot.plugin import TradeOgreBotPlugin
 class Back(TradeOgreBotPlugin):
 
     def get_handlers(self):
-        return [self._get_back_handler(), self._get_cmd_handler()]
+        return [self._get_back_handler()]
 
     def _get_back_handler(self):
         return RegexHandler(f"^({lbl.BTN_BACK})$", self.back)
-
-    def _get_cmd_handler(self):
-        return CommandHandler("back", self.back)
